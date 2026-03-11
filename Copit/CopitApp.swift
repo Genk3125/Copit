1f83e0e7aa244d0a7aac62dde0487c8797f419a9
近藤元樹 <modhi2525@icloud.com>
2026-03-12 02:29:32 +0900

    Initial Commit

---
+++ Copit/CopitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopitApp.swift
+//  Copit
+//
+//  Created by 近道元樹 on 2026/03/12.
+//
+
+import SwiftUI
+
+@main
+struct CopitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
